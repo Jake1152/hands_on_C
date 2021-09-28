0b51af9c11132068c6f3f567e44a8413f3e4dfb9
Jake <rancho974@gmail.com>
2021-09-28 23:12:30 +0900

    chapter 9 first try

---
+++ ch9/ch_9_1_need_function.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("************\n");
+    printf("    Jinho IM\n");
+    printf("    Seoul, Korea\n");    
+    printf("************\n");
+    
+    return (0);
+}
