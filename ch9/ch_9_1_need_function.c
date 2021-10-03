1da234fd8f38cc40552363a95c6ee080b61e8813
Jinho <rancho974@gmail.com>
2021-10-03 22:30:56 +0900

    ch_9_1

--- ch9/ch_9_1_need_function.c
+++ ch9/ch_9_1_need_function.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <string.h> // strlen()
+
+void    print_star(int n);
+
 
 int main()
 {
-    printf("************\n");
-    printf("    Jinho IM\n");
-    printf("    Seoul, Korea\n");    
-    printf("************\n");
-    
-    return (0);
+	int n;
+	scanf("%d", &n);
+
+	print_star(n);
+	printf("    Jinho IM\n");
+	printf("    Seoul, Korea\n");    
+	printf("************\n");
+	/*
+	변형가능한 형태로 만든다.
+	- 별의 갯수가 변경될떄
+		- 같은 코드가 2번 나옴
+		- 함수로 변경
+	
+	*/
+	
+	return (0);
+}
+
+void    print_star(int n)
+{
+	for (int i = 0; i < n; i++)
+		putchar("*");
+	putchar("\n");
 }
