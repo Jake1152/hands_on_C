1e7c55b4097d9112e8e2f24511db088c7d4164a2
Jake <rancho974@gmail.com>
2021-11-06 00:00:20 +0900

    11.11

---
+++ ch11/ch_11_11_string_to_num.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char *argv[])
+{
+	/*
+		string to integer, double, long
+		atoi(), atof(), atol()
+	*/
+
+	if (argc < 3)
+		printf("Wrong Usage of %s\n", argv[0]);
+	else
+	{
+		/* Example 1 */
+		// int times = atoi(argv[1]);
+		// // atof(), atol()
+
+		// for (int i = 0; i < times; i++)
+		//     puts(argv[2]);
+
+		/* Example2  */
+		printf("Sum = %d\n", atoi(argv[1]) + atoi(argv[2]));
+	}
+	/*
+		strint to long, unsigned long, double
+		strtol(), strtoul(), strtod()
+	*/ 
+
+	char str1[] = "-1024Hello";
+	char *end;
+	long l = sttol(str1, &end, 10);
+	printf("%s %;d %s %d\n", str1, l, end, (int)*end);
+
+	char str2[] =
+	long  ; - strtol(str2, &end, 16)
+
+	return (0);
+}
